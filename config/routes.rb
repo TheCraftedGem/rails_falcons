10ac7c38982f9ea8ba34c05ee79c5ccf026eeb5c
Emilio <thecraftedgem@tuta.io>
2019-02-26 14:58:39 -0700

    Adds User Log In And Create

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
         get '/news', to: 'news#index'
+        post '/users', to: "users#create"
+        post '/sessions', to: "sessions#create"
     end
   end
 end
